bdc3f2dedaaf26f4f19d7a7665917baf6d85b706
Cleyton Messias <cleytonmessias@gmail.com>
2018-05-01 19:08:40 -0300

    correcting no loop

--- cs231n/classifiers/k_nearest_neighbor.py
+++ cs231n/classifiers/k_nearest_neighbor.py
@@ -132,7 +132,8 @@ class KNearestNeighbor(object):
     test_sum = np.sum(np.square(X),axis=1)
     train_sum = np.sum(np.square(self.X_train),axis=1)
     inner_product  = -2 * np.dot(X,self.X_train.T)
-    dists = np.sqrt(train_sum + test_sum.reshape(-1, 1)  + inner_product)
+    # dists = np.sqrt(train_sum + test_sum.reshape(-1, 1)  + inner_product)
+    dists = np.sqrt(-2 * inner_product + test_sum.reshape(-1, 1) + train_sum) # broadcas
 
 
     #########################################################################
