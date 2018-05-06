1e010c6e4e4fb64cb5ac065f5ff397e6333031d3
Cleyton Messias <cleytonmessias@gmail.com>
2018-05-06 17:59:12 -0300

    calculating GD

--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -33,14 +33,18 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
+        dW[:,j] += X[i,:]
+        dW[:,y[i]] -= X[i,:]
         loss += margin
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dw =/ num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dw += reg * W
 
   #############################################################################
   # TODO:                                                                     #
